1c44e7c919c2e31537d89e1b3c013bacb9017121
mslmyrtd <mslmyrtd@gmail.com>
2023-01-12 23:02:23 +0300

    type adde to context and added some functionalty

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,11 +1,33 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
+import mockFollowers from "./mockData/mockFollowers";
+import mockRepos from "./mockData/mockRepos";
+import mockUser from "./mockData/mockUser";
+import axios from "axios";
+
+const rootUrl = 'https://api.github.com';
 
 interface InputProviderProps {
     children: React.ReactNode
 }
-const GithubContext = createContext("");
+interface GithubContextInterface {
+    githubUser: Object
+    repos: Array<object>
+    followers: Array<object>
+}
+const initialContext = {
+    githubUser: mockUser,
+    repos: mockRepos,
+    followers: mockFollowers
+}
+const GithubContext = createContext<GithubContextInterface>(initialContext);
 
 const GithubProvider = ({ children }: InputProviderProps) => {
-    return (<GithubContext.Provider value={"hello"}>{children}</GithubContext.Provider>)
+    const [githubUser, setGithubUser] = useState(mockUser)
+    const [repos, setRepos] = useState(mockRepos)
+    const [followers, setFollowers] = useState(mockFollowers)
+    return (<GithubContext.Provider value={{ githubUser, repos, followers }}>{children}</GithubContext.Provider>)
 }
-export { GithubContext, GithubProvider }
+const useGlobalContext = () => {
+    return useContext(GithubContext)
+}
+export { GithubContext, GithubProvider, useGlobalContext }
