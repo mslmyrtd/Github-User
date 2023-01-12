4086403e105fc03d0dfa7d678f3a7a3c93cbd81b
mslmyrtd <mslmyrtd@gmail.com>
2023-01-12 18:01:46 +0300

    created mockData folder and my github information added

---
+++ src/context/mockData/mockUser.tsx
@@ -0,0 +1,37 @@
+export default {
+    login: "mslmyrtd",
+    id: 87023339,
+    node_id: 'MDQ6VXNlcjg3MDIzMzM5',
+    avatar_url: 'https://avatars.githubusercontent.com/u/87023339?v=4',
+    gravatar_id: '',
+    url: 'https://api.github.com/users/mslmyrtd',
+    html_url: 'https://github.com/mslmyrtd',
+    followers_url: 'https://api.github.com/users/mslmyrtd/followers',
+    following_url:
+        'https://api.github.com/users/mslmyrtd/following{/other_user}',
+    gists_url: 'https://api.github.com/users/mslmyrtd/gists{/gist_id}',
+    starred_url:
+        'https://api.github.com/users/mslmyrtd/starred{/owner}{/repo}',
+    subscriptions_url: 'https://api.github.com/users/mslmyrtd/subscriptions',
+    organizations_url: 'https://api.github.com/users/mslmyrtd/orgs',
+    repos_url: 'https://api.github.com/users/mslmyrtd/repos',
+    events_url: 'https://api.github.com/users/mslmyrtd/events{/privacy}',
+    received_events_url:
+        'https://api.github.com/users/mslmyrtd/received_events',
+    type: 'User',
+    site_admin: false,
+    name: 'Muslum Yurtada',
+    company: 'Freelancer',
+    blog: 'www.mslmyrtd.com',
+    location: 'Izmir',
+    email: "mslmyrtd@gmail.com",
+    hireable: "yes",
+    bio: 'Strong Man',
+    twitter_username: 'Mslm',
+    public_repos: 47,
+    public_gists: 0,
+    followers: 5,
+    following: 9,
+    created_at: "2021-07-06T13:46:22Z",
+    updated_at: "2022-12-03T13:44:19Z"
+};
