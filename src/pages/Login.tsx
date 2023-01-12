f6d1a895e15e957f0282196d5ad3f7b9c7f8a05a
mslmyrtd <mslmyrtd@gmail.com>
2023-01-12 16:23:45 +0300

    login components structure created and added styled coponent

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import logimImg from '../images/login-img.svg'
 const Login = () => {
     return (
-        <div>
-            login
-        </div>
+        <Wrapper>
+            <div className='container'>
+                <img src={logimImg} alt='login' />
+                <h1>github user</h1>
+                <button className='btn'>login</button>
+            </div>
+        </Wrapper>
     )
 }
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  .container {
+    width: 90vw;
+    max-width: 600px;
+    text-align: center;
+  }
 
+  img {
+    margin-bottom: 2rem;
+  }
+  h1 {
+    margin-bottom: 1.5rem;
+  }
+`
 export default Login
