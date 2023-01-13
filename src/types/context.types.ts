00c274f225b686594e25658bc13a9490c095f499
mslmyrtd <mslmyrtd@gmail.com>
2023-01-13 21:12:37 +0300

    added new types

--- src/types/context.types.ts
+++ src/types/context.types.ts
@@ -3,4 +3,12 @@ export interface IGithubUser {
   followers: number
   following: number
   public_gists: number
+  avatar_url: string
+  html_url: string
+  name: string
+  company: string
+  blog: string
+  bio: string
+  location: string
+  twitter_username: string
 }
