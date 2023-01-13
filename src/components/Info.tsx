105b856a29c604b45d4408ff3fd3e6563c336b1c
mslmyrtd <mslmyrtd@gmail.com>
2023-01-13 21:12:12 +0300

    info.tsx structure completed

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useGlobalContext } from '../context/context';
 import styled from 'styled-components';
 import { GoRepo, GoGist } from 'react-icons/go';
@@ -6,55 +5,55 @@ import { FiUsers, FiUserPlus } from 'react-icons/fi';
 import { IItem } from '../types/type.types';
 
 const UserInfo = () => {
-    const { githubUser } = useGlobalContext()
-    const { public_repos, followers, following, public_gists } = githubUser
-    console.log(public_repos);
-    const items: IItem[] = [
-        {
-            id: 1,
-            icon: <GoRepo className="icon" />,
-            label: "repos",
-            value: public_repos,
-            color: "pink"
-        },
-        {
-            id: 2,
-            icon: <FiUsers className="icon" />,
-            label: "followers",
-            value: followers,
-            color: "green"
-        },
-        {
-            id: 3,
-            icon: <FiUserPlus className="icon" />,
-            label: "following",
-            value: following,
-            color: "purple"
-        },
-        {
-            id: 4,
-            icon: <GoGist className="icon" />,
-            label: "gists",
-            value: public_gists,
-            color: "yellow"
-        },
-    ]
-    return <section className='section'>
-        <Wrapper className='section-center'>
-            {items.map((item) => {
-                return <Item key={item.id} {...item}></Item>
-            })}
-        </Wrapper>
-    </section>
+  const { githubUser } = useGlobalContext()
+  const { public_repos, followers, following, public_gists } = githubUser
+  console.log(public_repos);
+  const items: IItem[] = [
+    {
+      id: 1,
+      icon: <GoRepo className="icon" />,
+      label: "repos",
+      value: public_repos,
+      color: "pink"
+    },
+    {
+      id: 2,
+      icon: <FiUsers className="icon" />,
+      label: "followers",
+      value: followers,
+      color: "green"
+    },
+    {
+      id: 3,
+      icon: <FiUserPlus className="icon" />,
+      label: "following",
+      value: following,
+      color: "purple"
+    },
+    {
+      id: 4,
+      icon: <GoGist className="icon" />,
+      label: "gists",
+      value: public_gists,
+      color: "yellow"
+    },
+  ]
+  return <section className='section'>
+    <Wrapper className='section-center'>
+      {items.map((item) => {
+        return <Item key={item.id} {...item}></Item>
+      })}
+    </Wrapper>
+  </section>
 }
 const Item = ({ icon, label, value, color }: any) => {
-    return <article className='item'>
-        <span className={color}>{icon}</span>
-        <div>
-            <h3>{value}</h3>
-            <p>{label}</p>
-        </div>
-    </article>
+  return <article className='item'>
+    <span className={color}>{icon}</span>
+    <div>
+      <h3>{value}</h3>
+      <p>{label}</p>
+    </div>
+  </article>
 }
 const Wrapper = styled.section`
   display: grid;
