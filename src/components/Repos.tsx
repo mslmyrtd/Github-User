58abc20bd77a300a34d4b4bfa60108cd6af2c77c
mslmyrtd <mslmyrtd@gmail.com>
2023-01-15 22:01:05 +0300

    added new function to get those starts for each languages and send with props to doughnut2D componnet

--- src/components/Repos.tsx
+++ src/components/Repos.tsx
@@ -7,33 +7,41 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const { repos } = useGlobalContext()
   //reach to laanguages
-  let languages = repos.reduce((total, item) => {
+  const languages = repos.reduce((total, item) => {
     //@ts-ignore
-    const { language } = item
+    const { language, stargazers_count } = item
     if (!language) return total
     //@ts-ignore
     if (!total[language]) {
       //@ts-ignore
-      total[language] = { label: language, value: 1 }
+      total[language] = { label: language, value: 1, starts: stargazers_count }
     } else {
       //@ts-ignore
-      total[language] = { ...total[language], value: total[language].value + 1 }
+      total[language] = { ...total[language], value: total[language].value + 1, starts: total[language].stars + stargazers_count }
     }
     //@ts-ignore
     return total
   }, {})
-  languages = Object.values(languages).sort((a, b) => {
+  console.log(languages);
+  const mostUsed = Object.values(languages).sort((a, b) => {
     return b.value - a.value
   }).slice(0, 5)
-  console.log(languages);
 
+  //most starts per language
 
-
+  const mostPopular = Object.values(languages).sort((a, b) => {
+    return b.stars - a.stars
+  }).map((item) => {
+    return { ...item, value: item.stars }
+  }).slice(0, 5)
 
   return <section>
     <Wrapper className='section-center'>
       {/* @ts-ignore */}
-      <Pie3D data={languages} />
+      <Pie3D data={mostUsed} />
+      <div></div>
+      {/* @ts-ignore */}
+      <Doughnut2D data={mostPopular} />
     </Wrapper>
   </section>;
 
