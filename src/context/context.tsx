0c4f8d219fc17c6ef8e9d4bc4abaabbb9b16cdcc
mslmyrtd <mslmyrtd@gmail.com>
2023-01-16 15:17:10 +0300

    added new variable for calculating how many request send and left and send to search comp.

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import mockFollowers from "./mockData/mockFollowers";
 import mockRepos from "./mockData/mockRepos";
 import mockUser from "./mockData/mockUser";
@@ -14,18 +14,38 @@ interface GithubContextInterface {
     githubUser: IGithubUser
     repos: Array<object>
     followers: IFollowers[]
+    requests: number
 }
 const initialContext = {
     githubUser: mockUser,
     repos: mockRepos,
-    followers: mockFollowers
+    followers: mockFollowers,
+    requests: 0
 }
 const GithubContext = createContext<GithubContextInterface>(initialContext);
 const GithubProvider = ({ children }: InputProviderProps) => {
     const [githubUser, setGithubUser] = useState(mockUser)
     const [repos, setRepos] = useState(mockRepos)
     const [followers, setFollowers] = useState(mockFollowers)
-    return (<GithubContext.Provider value={{ githubUser, repos, followers }}>{children}</GithubContext.Provider>)
+    //request loading
+    const [requests, setRequests] = useState(0)
+    const [loading, setLoading] = useState(false)
+    //check rate
+    const checkRequest = () => {
+        axios(`${rootUrl}/rate_limit`).then(({ data }) => {
+            let { rate: { remaining } } = data
+            setRequests(remaining)
+            if (remaining === 0) {
+                //throw an erro
+            }
+        }).catch((err) => console.log(err))
+    }
+    //error
+    useEffect(
+        checkRequest
+
+        , [])
+    return (<GithubContext.Provider value={{ githubUser, repos, followers, requests }}>{children}</GithubContext.Provider>)
 }
 const useGlobalContext = () => {
     return useContext(GithubContext)
