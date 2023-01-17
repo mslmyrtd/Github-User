d6e92019ddb2fca502f0cd086484f1c05fac3675
mslmyrtd <mslmyrtd@gmail.com>
2023-01-17 14:08:59 +0300

    loading added and image

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
-import { Info, User, Repos, Search } from '../components'
+import { Info, User, Repos, Search, Navbar } from '../components'
 import { useGlobalContext } from '../context/context'
 import loadingImage from '../images/preloader.gif'
 const Dashboard = () => {
-
+    const { isLoading } = useGlobalContext()
+    if (isLoading) {
+        return <main>
+            <Navbar />
+            <Search />
+            <img src={loadingImage} alt="loading" className='loading-img' />
+        </main>
+    }
     return (
-        <div>
-            dashboard
+        <main>
+            <Navbar />
             <Search />
             <Info />
             <User />
             <Repos />
-        </div>
+        </main>
     )
 }
 
