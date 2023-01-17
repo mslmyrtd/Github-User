ff9aa67b8b60241b2e95a21bed979205c83fc60f
mslmyrtd <mslmyrtd@gmail.com>
2023-01-17 22:39:02 +0300

    PrivateRoute componnet added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 import Error from "./Error";
+import PrivateRoute from "./PrivateRoute";
 
-export { Dashboard, Login, Error };
+export { Dashboard, Login, Error, PrivateRoute };
