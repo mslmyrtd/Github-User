aa64ffcd66863b15a38992e9e462ed106fa2ea5b
mslmyrtd <mslmyrtd@gmail.com>
2023-01-17 21:49:15 +0300

    added login logout button and show user name and picture

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useAuth0 } from "@auth0/auth0-react"
+import React from 'react'
+import styled from 'styled-components'
+import { useAuth0 } from '@auth0/auth0-react'
 const Navbar = () => {
-  const { loginWithRedirect, isAuthenticated, logout, user, isLoading } = useAuth0();
-  return <Wrapper>
-    <button onClick={() => loginWithRedirect()}>Log In</button>
-  </Wrapper>;
-};
+  const { loginWithRedirect, isAuthenticated, logout, user, isLoading } =
+    useAuth0()
+  console.log({ user, isLoading, isAuthenticated })
+  const isUser = isAuthenticated && user
+  return (
+    <Wrapper>
+      {isUser && user.picture && <img src={user.picture} alt={user.name} />}
+      {isUser && user.name && (
+        <h4>
+          Welcome, <strong>{user.name.toUpperCase()}</strong>
+        </h4>
+      )}
+      {isUser ? (
+        <button onClick={() => logout({ returnTo: window.location.origin })}>
+          Log Out
+        </button>
+      ) : (
+        <button onClick={loginWithRedirect}>Log In</button>
+      )}
+    </Wrapper>
+  )
+}
 
 const Wrapper = styled.nav`
   padding: 1.5rem;
@@ -37,6 +54,6 @@ const Wrapper = styled.nav`
     color: var(--clr-grey-5);
     cursor: pointer;
   }
-`;
+`
 
-export default Navbar;
+export default Navbar
