07d827961625f42d9d7a67f3832f161bd9e8b8c5
mslmyrtd <mslmyrtd@gmail.com>
2023-01-17 22:38:37 +0300

    PrivateRoute wrapped dashboard for control to login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Dashboard, Login, Error } from './pages';
+import { Dashboard, Login, Error, PrivateRoute } from './pages';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <PrivateRoute>
+          <Route path="/" element={<Dashboard />} />
+        </PrivateRoute>
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<Error />} />
       </Routes>
