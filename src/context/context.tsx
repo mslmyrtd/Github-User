8d7b5894b1fa774f9812e0a14d2b21273abec368
mslmyrtd <mslmyrtd@gmail.com>
2023-01-17 14:50:01 +0300

    added Promise.allSettled for sure that all data comes back at the same time

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,11 +1,11 @@
-import { createContext, useContext, useState, useEffect } from "react";
-import mockFollowers from "./mockData/mockFollowers";
-import mockRepos from "./mockData/mockRepos";
-import mockUser from "./mockData/mockUser";
-import axios from "axios";
-import { IGithubUser, IFollowers } from "../types/context.types";
+import { createContext, useContext, useState, useEffect } from 'react'
+import mockFollowers from './mockData/mockFollowers'
+import mockRepos from './mockData/mockRepos'
+import mockUser from './mockData/mockUser'
+import axios from 'axios'
+import { IGithubUser, IFollowers } from '../types/context.types'
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = 'https://api.github.com'
 
 interface InputProviderProps {
     children: React.ReactNode
@@ -15,7 +15,7 @@ interface GithubContextInterface {
     repos: Array<object>
     followers: IFollowers[]
     requests: number
-    error: { show: boolean, msg: string }
+    error: { show: boolean; msg: string }
     searchGithubUser: Function
     isLoading: boolean
 }
@@ -25,13 +25,14 @@ const initialContext = {
     followers: mockFollowers,
     requests: 0,
     error: {
-        show: false, msg: ""
+        show: false,
+        msg: '',
     },
     searchGithubUser: Function,
-    isLoading: false
+    isLoading: false,
 }
 
-const GithubContext = createContext<GithubContextInterface>(initialContext);
+const GithubContext = createContext<GithubContextInterface>(initialContext)
 const GithubProvider = ({ children }: InputProviderProps) => {
     const [githubUser, setGithubUser] = useState(mockUser)
     const [repos, setRepos] = useState(mockRepos)
@@ -40,25 +41,33 @@ const GithubProvider = ({ children }: InputProviderProps) => {
     const [requests, setRequests] = useState(0)
     const [isLoading, setIsLoading] = useState(false)
     //error
-    const [error, setError] = useState({ show: false, msg: "" })
+    const [error, setError] = useState({ show: false, msg: '' })
 
     const searchGithubUser = async (user: string): Promise<any> => {
         toggleError()
         setIsLoading(true)
-        const response = await axios(`${rootUrl}/users/${user}`).catch((err) => console.log(err)
+        const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+            console.log(err)
         )
         if (response) {
             if (response.status === 200) {
-                console.log("basarılı")
                 setGithubUser(response.data)
                 const { login, followers_url } = response.data
-                //repos
-                axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((res) => setRepos(res.data))
-                //followers
-                axios(`${followers_url}?per_page=100`).then((res: any) => setFollowers(res.data))
+                await Promise.allSettled([
+                    axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+                    axios(`${followers_url}?per_page=100`),
+                ]).then((results) => {
+                    const [repos, followers] = results;
+                    const status = 'fulfilled';
+                    if (repos.status === status) {
+                        setRepos(repos.value.data);
+                    }
+                    if (followers.status === status) {
+                        setFollowers(followers.value.data);
+                    }
+                }).catch(err => console.log(err))
             } else {
-                console.log("error");
-                toggleError(true, "there is no user with that username")
+                toggleError(true, 'there is no user with that username')
             }
             checkRequest()
             setIsLoading(false)
@@ -66,23 +75,42 @@ const GithubProvider = ({ children }: InputProviderProps) => {
     }
     //check rate
     const checkRequest = () => {
-        axios(`${rootUrl}/rate_limit`).then(({ data }) => {
-            let { rate: { remaining } } = data
-            setRequests(remaining)
-            if (remaining === 0) {
-                toggleError(true, "sorry, you have exceeded your hourly rate limit")
-            }
-        }).catch((err) => console.log(err))
+        axios(`${rootUrl}/rate_limit`)
+            .then(({ data }) => {
+                let {
+                    rate: { remaining },
+                } = data
+                setRequests(remaining)
+                if (remaining === 0) {
+                    toggleError(true, 'sorry, you have exceeded your hourly rate limit')
+                }
+            })
+            .catch((err) => console.log(err))
     }
-    function toggleError(show: boolean = false, msg: string = "") {
+    function toggleError(show: boolean = false, msg: string = '') {
         setError({ show, msg })
     }
     //error
     useEffect(
-        checkRequest
+        checkRequest,
 
-        , [])
-    return (<GithubContext.Provider value={{ githubUser, repos, followers, requests, error, searchGithubUser, isLoading }}>{children}</GithubContext.Provider>)
+        []
+    )
+    return (
+        <GithubContext.Provider
+            value={{
+                githubUser,
+                repos,
+                followers,
+                requests,
+                error,
+                searchGithubUser,
+                isLoading,
+            }}
+        >
+            {children}
+        </GithubContext.Provider>
+    )
 }
 const useGlobalContext = () => {
     return useContext(GithubContext)
