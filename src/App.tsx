cd01beb390f552a4dcd55cab53e6d8375733f6fd
mslmyrtd <mslmyrtd@gmail.com>
2023-01-18 11:03:15 +0300

    Added PrivtateRoute for see dasboard user must be login

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { Dashboard, Login, Error, PrivateRoute } from './pages';
+import React, { Fragment } from 'react';
+import { Dashboard, Login, Error, PrivateRoute, AuthWrapper } from './pages';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <PrivateRoute>
-          <Route path="/" element={<Dashboard />} />
-        </PrivateRoute>
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Fragment>
+          <Routes>
+            <Route path='/' element={<PrivateRoute />}>
+              <Route path='/' element={<Dashboard />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Fragment>
 
+      </Router>
+    </AuthWrapper>
   );
 }
 
