25c6765f2724172d00f41df0086a19c907885684
mslmyrtd <mslmyrtd@gmail.com>
2023-01-18 14:38:27 +0300

    new property added

--- src/components/Charts/Doughnut2d.tsx
+++ src/components/Charts/Doughnut2d.tsx
@@ -26,9 +26,27 @@ const ChartComponent = ({ data }) => {
         dataSource: {
             // Chart Configuration
             chart: {
-                caption: 'Starts Per Language',
-                doughnutRadius: '40%',
+                caption: "Stars Per Language",
+                decimals: 0,
+                pieRadius: "45%",
+                doughnutRadius: "60%",
                 showPercentValues: 0,
+                captionFontColor: "#102a42",
+                captionFontBold: 0,
+                captionFontSize: 20,
+                captionFont: "Roboto",
+                baseFont: "Open Sans",
+                baseFontSize: 16,
+                baseFontColor: "#617d98",
+                smartLineColor: "#617d98",
+                showShadow: 0,
+                showPlotBorder: 0,
+                paletteColors:
+                    "#2caeba, #5D62B5, #FFC533, #F2726F, #8d6e63, #1de9b6, #6E80CA",
+                use3DLighting: 0,
+                useDataPlotColorForLabels: 0,
+                bgColor: "#FFFFFF",
+                showBorder: 0,
                 theme: 'candy'
             },
             // Chart Data
