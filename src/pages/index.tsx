ae09617db507b0e64ed7067e752416f685036a01
mslmyrtd <mslmyrtd@gmail.com>
2023-01-18 11:05:15 +0300

    AuthWrapper added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,6 @@ import Dashboard from "./Dashboard";
 import Login from "./Login";
 import Error from "./Error";
 import PrivateRoute from "./PrivateRoute";
+import AuthWrapper from "./AuthWrapper";
 
-export { Dashboard, Login, Error, PrivateRoute };
+export { Dashboard, Login, Error, PrivateRoute, AuthWrapper };
